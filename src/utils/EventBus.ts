dcabc374564c1abcb6d8ce08b173a407c6c20886
alexTaraskina <serzhanina_aleks@mail.ru>
2022-12-26 13:07:24 +0300

    add core modules

---
+++ src/utils/EventBus.ts
@@ -0,0 +1,45 @@
+// type PageEventKey = 'load' | 'ready';
+
+export type Listener<T extends unknown[] = any[]> = (...args: T) => void;
+
+export default class EventBus<E extends string = string, M extends { [K in E]: unknown[] } = Record<E, any[]>> {
+  private listeners: { [key in E]?: Listener<M[E]>[] } = {};
+
+  on(event: E, callback: Listener<M[E]>) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event]!.push(callback);
+  }
+
+  off(event: E, callback: Listener<M[E]>) {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event] = this.listeners[event]!.filter(
+      listener => listener !== callback,
+    );
+  }
+
+  emit(event: E, ...args: M[E]) {
+    if (!this.listeners[event]) {
+      return;
+    }
+
+    this.listeners[event]!.forEach(function (listener) {
+      listener(...args);
+    });
+  }
+}
+
+// type DocumentEventBus = EventBus<
+//     PageEventKey, 
+//     {
+//         ['load']: [number, number],
+//         ['ready']: []
+//     }
+// >
+
+// const myListener: DocumentEventBus = new EventBus();
