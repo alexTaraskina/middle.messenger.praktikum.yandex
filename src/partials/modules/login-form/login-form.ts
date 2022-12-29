1dacb2f8fdbf099ab0f8f66d58dd94b65d85842e
alexTaraskina <serzhanina_aleks@mail.ru>
2022-12-30 01:01:31 +0300

    alias

--- src/partials/modules/login-form/login-form.ts
+++ src/partials/modules/login-form/login-form.ts
@@ -1,8 +1,16 @@
-import { Block } from '../../../utils';
+import { Block } from 'utils';
 import template from 'bundle-text:./template.hbs';
 
 export default class LoginForm extends Block {
     static componentName: string = 'LoginForm';
+
+    constructor() {
+        super();
+
+        this.setProps({
+            onsubmit: () => { console.log('submit'); }
+        });
+    }
     
     render() {
         return template;
