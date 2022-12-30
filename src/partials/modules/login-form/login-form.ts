efec618bb40673a3a6621ae5505b2ae02dea9f36
alexTaraskina <serzhanina_aleks@mail.ru>
2022-12-30 14:05:46 +0300

    capture form data

--- src/partials/modules/login-form/login-form.ts
+++ src/partials/modules/login-form/login-form.ts
@@ -1,17 +1,41 @@
 import { Block } from 'utils';
 import template from 'bundle-text:./template.hbs';
 
-export default class LoginForm extends Block {
+interface LoginFormProps {
+}
+
+export default class LoginForm extends Block<LoginFormProps> {
     static componentName: string = 'LoginForm';
 
-    constructor() {
-        super();
+    constructor(props: LoginFormProps) {
+        super(props);
 
         this.setProps({
-            onsubmit: () => { console.log('submit'); }
+            events: {
+                submit: (event: MouseEvent) => this.onSubmit(event)
+            }
         });
     }
-    
+
+    onSubmit(event: MouseEvent) {
+        event.preventDefault();
+
+        interface LoginData {
+            login: string,
+            password: string
+        };
+
+        const loginEl = this.element?.querySelector('#login') as HTMLInputElement;
+        const passwordEl = this.element?.querySelector('#password') as HTMLInputElement;
+
+        const loginData: LoginData = {
+            login: loginEl?.value,
+            password: passwordEl?.value,
+        }
+
+        console.log(loginData);
+    }
+
     render() {
         return template;
     }
--- src/partials/modules/register-form/register-form.ts
+++ src/partials/modules/register-form/register-form.ts
@@ -1,8 +1,53 @@
 import { Block } from '../../../utils';
 import template from 'bundle-text:./template.hbs';
 
-export default class RegisterForm extends Block {
+interface RegisterFormProps {
+
+}
+
+export default class RegisterForm extends Block<RegisterFormProps> {
     static componentName: string = 'RegisterForm';
+
+    constructor(props: RegisterFormProps) {
+        super(props);
+
+        this.setProps({
+            events: {
+                submit: (event: MouseEvent) => this.onSubmit(event)
+            }
+        });
+    }
+
+    onSubmit(event: MouseEvent) {
+        event.preventDefault();
+
+        interface RegisterData {
+            email: string,
+            login: string,
+            firstName: string,
+            secondName: string,
+            phone: string,
+            password: string
+        };
+
+        const emailEl = this.element?.querySelector('#email') as HTMLInputElement;
+        const loginEl = this.element?.querySelector('#login') as HTMLInputElement;
+        const firstNameEl = this.element?.querySelector('#firstName') as HTMLInputElement;
+        const secondNameEl = this.element?.querySelector('#secondName') as HTMLInputElement;
+        const phoneEl = this.element?.querySelector('#phone') as HTMLInputElement;
+        const passwordEl = this.element?.querySelector('#password') as HTMLInputElement;
+
+        const registerData: RegisterData = {
+            email: emailEl?.value,
+            login: loginEl?.value,
+            firstName: firstNameEl?.value,
+            secondName: secondNameEl?.value,
+            phone: phoneEl?.value,
+            password: passwordEl?.value,
+        }
+
+        console.log(registerData);
+    }
     
     render() {
         return template;
